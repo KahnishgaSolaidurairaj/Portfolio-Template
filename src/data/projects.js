b2a846b0f562e2413381d20016e63476fb934d10
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-04-14 01:14:27 -0500

    Blank Template

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: "Title of Game",
+    description: "Short Description of Game",
+    image: "/public/images/square.avif",
+    tags: ["Language", "Framework", "Topic"],
+    github: "https://link",
+    // demo: null
+  },
+  {
+    title: "Title of Game",
+    description: "Short Description of Game",
+    image: "/public/images/square.avif",
+    tags: ["Language", "Framework", "Topic"],
+    github: "https://link",
+    demo: "https://link"
+  },
+  {
+    title: "Title of Game",
+    description: "Short Description of Game",
+    image: "/public/images/square.avif",
+    tags: ["Language", "Framework", "Topic"],
+    github: "https://link",
+    demo: "https://link"
+  },
+  {
+    title: "Title of Game",
+    description: "Short Description of Game",
+    image: "/public/images/square.avif",
+    tags: ["Language", "Framework", "Topic"],
+    github: "https://link",
+    demo: "https://link"
+  },
+  {
+    title: "Title of Game",
+    description: "Short Description of Game",
+    image: "/public/images/square.avif",
+    tags: ["Language", "Framework", "Topic"],
+    github: "https://link",
+    demo: "https://link"
+  }
+];
---
+++ src/data/skills.js
@@ -0,0 +1,5 @@
+export default {
+  languages: ["JavaScript", "HTML", "CSS", "other languages"],
+  frameworks: ["Astro", "other frameworks"],
+  coursework: ["Data Structures", "course name"]
+};
